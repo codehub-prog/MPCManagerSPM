1d2f14287fb20d13459c4fcf00c169217cb41e25
Anshul Kumar <kumaranshul1798@gmail.com>
2026-03-17 15:59:28 +0530

    added empty file to set target

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(
-            name: "MPCManagerSPM",
+            name: "MPCManagerFramework",
             url: "https://github.com/codehub-prog/MPCManagerSPM/releases/download/1.0.6/MPCManagerFramework.xcframework.zip",
             checksum: "931e898621105bb137f9436203d4ff27545df6a942db10e158c040a7ca63e049"
         ),
