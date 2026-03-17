d56ca2d6f9462a58116a18d390b290395856d8e0
Anshul Kumar <kumaranshul1798@gmail.com>
2026-03-17 14:52:50 +0530

    updated new tag

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MPCManagerSPM",
-            url: "https://github.com/codehub-prog/MPCManagerFramework/releases/download/1.0.3/MPCManagerFramework.xcframework.zip",
+            url: "https://github.com/codehub-prog/MPCManagerFramework/releases/download/1.0.4/MPCManagerFramework.xcframework.zip",
             checksum: "2d74c5c808fddfbee552344cccf9506ff1d2afeeb9d1df9b3fcd3fa80e185c10"
         )
     ]
